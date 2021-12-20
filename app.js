47c24f430678ddfe7d297297f38082675f11884f
Keith Hall <hkeith62@gmail.com>
2021-12-20 17:54:23 -0600

    Index path added

--- app.js
+++ app.js
@@ -51,7 +51,14 @@ var options = {
 
 const openapiSpecification = swaggerJsdoc(options); // Options definitions are converted into swagger docs and held in variable.
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(openapiSpecification, {explorer: true})); // Serve Swagger specification at api- docs, Explorer api search.
+app.get("/", function (request, response) {
+
+    response.render("api-docs", {
+        title: "Swagger Docs"
+    });
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification, {explorer: true})); // Serve Swagger specification at api- docs, Explorer api search.
 app.use('/api', teamAPI);
 
 http.createServer(app).listen(app.get('port'), function() {
